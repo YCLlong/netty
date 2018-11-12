6793ab96f8f4b9ba64e864024ddc65e175822c39
YCLlong <251121753@qq.com>
2018-11-12 23:51:40 +0800

    Initial commit

---
+++ src/main/java/cn/ycl/nio/MyClient.java
@@ -0,0 +1,102 @@
+package cn.ycl.nio;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * 客户端设计
+ * 1，创建SocketChannel,去连接服务器
+ *
+ *
+ *
+ *
+ */
+public class MyClient {
+    private String remoteIp;//远程ip
+    private int remotePort;//远程端口
+    private SocketChannel socketChannel;
+    private Selector selector;
+    private ByteBuffer readBuffer;//读缓冲器
+    private ByteBuffer writeBuffer;//写缓冲区
+
+    public MyClient(String remoteIp,int remotePort) throws IOException {
+        readBuffer = ByteBuffer.allocate(1024);//1k的缓冲区，容量有限，当读取大于1k的数据就读取不到
+        writeBuffer = ByteBuffer.allocate(1024);
+        this.remoteIp = remoteIp;
+        this.remotePort = remotePort;
+        //开启多路复用轮询器
+        selector = Selector.open();
+        socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, SelectionKey.OP_CONNECT);//注册连接成功事件
+        try {
+            socketChannel.connect(new InetSocketAddress(remoteIp,remotePort));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("连接服务器失败");
+        }
+        System.out.println("客户端启动成功");
+        listen();//开始监听
+    }
+
+    private void listen() throws IOException {
+        while (true){
+            selector.select(1000);//选择一组
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> it = selectionKeys.iterator();
+            if(it.hasNext()){
+                SelectionKey key = it.next();
+                it.remove();
+                handle(key);
+            }
+        }
+    }
+
+    private void handle(SelectionKey key) throws IOException {
+        SocketChannel client = (SocketChannel) key.channel();
+        if(key.isConnectable()){
+            System.out.println("服务器同意了你的连接");
+            // 判断此通道上是否正在进行连接操作。
+            // 完成套接字通道的连接过程。
+            if (client.isConnectionPending()) {
+                client.finishConnect();
+                System.out.println("完成连接!");
+                writeBuffer.clear();
+                writeBuffer.put("Hello,Server".getBytes());
+                writeBuffer.flip();
+                client.write(writeBuffer);
+            }
+            socketChannel.register(selector,SelectionKey.OP_READ);//注册读事件
+        }else if(key.isReadable()){
+            //有数据来了
+            readBuffer.clear();
+            int count = socketChannel.read(readBuffer);
+            String msg = new String(readBuffer.array(),0,count,"utf-8");
+            System.out.println("来自服务器的消息：" +  msg);
+            //注册写事件
+            socketChannel.register(selector,SelectionKey.OP_WRITE);
+        }else if(key.isWritable()){
+            System.out.println("请您输入发送到服务器的指令");
+            Scanner scanner = new Scanner(System.in);
+            String code = scanner.next();
+            writeBuffer.clear();
+            writeBuffer.put(code.getBytes("utf-8"));
+            writeBuffer.flip();
+            socketChannel.write(writeBuffer);
+            //注册读事件
+            socketChannel.register(selector,SelectionKey.OP_READ);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new MyClient("127.0.0.1",8080);
+    }
+}
---
+++ src/main/java/cn/ycl/nio/MyServer.java
@@ -0,0 +1,107 @@
+package cn.ycl.nio;
+
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * NIO服务器创建思路
+ * 1，创建服务器ServerSocketChannel
+ * 2，绑定端口，设置为非阻塞
+ * 3，将channel通道注册到多路复用Selector
+ * 4，开始轮询SelectKey（如果完成了某个事件就会被轮询到）
+ * 5，对于轮询到的key，进行处理
+ */
+public class MyServer {
+    private int port;//端口号
+    private ServerSocketChannel serverSocketChannel;//服务器channel通道
+    private ByteBuffer readBuffer;//读缓冲器
+    private ByteBuffer writeBuffer;//写缓冲区
+    private Selector selector;//轮询器
+    public MyServer(int port) throws IOException {
+        this.port = port;
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(port));
+        serverSocketChannel.configureBlocking(false);
+        selector = Selector.open();
+        serverSocketChannel.register(selector,SelectionKey.OP_ACCEPT);//注册请求连接时间
+        readBuffer = ByteBuffer.allocate(1024);//1k的缓冲区，容量有限，当读取大于1k的数据就读取不到
+        writeBuffer = ByteBuffer.allocate(1024);
+        System.out.println("服务器开启成功,监听端口：" + port );
+        listen();
+    }
+
+
+    public void listen() throws IOException {
+        while (true){
+            selector.select(1000);//选择一组
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> it = selectionKeys.iterator();
+            if(it.hasNext()){
+                SelectionKey key = it.next();
+                it.remove();
+                handle(key);
+            }
+        }
+    }
+
+    private void handle(SelectionKey key) throws IOException {
+        ServerSocketChannel server;
+        SocketChannel client;
+        if(key.isAcceptable()){
+            server = (ServerSocketChannel) key.channel();
+            client = server.accept();
+            client.configureBlocking(false);
+            System.out.println("连接成功。" + "ip地址：" + client.socket().getInetAddress().getHostAddress() + "端口号：" + client.socket().getPort());
+            //发送握手消息
+            writeBuffer.clear();
+            writeBuffer = ByteBuffer.wrap("success".getBytes("utf-8"));
+            writeBuffer.flip();
+            client.write(writeBuffer);
+            client.register(selector,SelectionKey.OP_READ);//注册读事件
+        }else if(key.isReadable()){
+            //有数据来了
+            client = (SocketChannel) key.channel();
+            readBuffer.clear();
+            int count =  client.read(readBuffer);
+            String code = new String(readBuffer.array(),0,count,"utf-8");
+            System.out.println("来自ip" + client.socket().getInetAddress().toString() + ":" + client.socket().getPort() + "的数据：" +  code);
+            //根据读取的指令返回客户端对应的信息
+            if("now".equals(code)){
+                //向客户端返回当前服务器时间
+                writeBuffer.clear();
+               // writeBuffer.put(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()).getBytes("utf-8"));
+                writeBuffer.put("123你好".getBytes());
+                writeBuffer.flip();
+                client.write(writeBuffer);
+            }else if("version".equals(code)){
+                writeBuffer.clear();
+                writeBuffer.put("v1.0  by ycl".getBytes());
+                writeBuffer.flip();
+                client.write(writeBuffer);
+            }else if("bye".equals(code)){
+                System.out.println("断开连接，ip" + client.socket().getInetAddress().toString() + ":" + client.socket().getPort());
+                client.close();
+            }else {
+                writeBuffer.clear();
+                writeBuffer.put("Imsorry".getBytes("UTF-8"));
+                writeBuffer.flip();
+                client.write(writeBuffer);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new MyServer(8080);
+    }
+}
